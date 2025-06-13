393de45cfad052de47ec8ddf7bd3a6e854cad0f7
soham-prabhu <157394259+soham-prabhu@users.noreply.github.com>
2025-06-13 17:05:26 +0530

    nineth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 // Pages
@@ -16,9 +16,30 @@ import Footer from './components/Footer/Footer.jsx';
 
 import './App.css';
 import ChatbotComponent from './components/Chatbot/ChatbotComponents.jsx';
+import DeveloperInfoPopup from './components/DeveloperInfo/DeveloperInfoPopup.jsx';
+import Myimg from './assets/WhatsApp Image 2025-06-13 at 16.48.01_dd2b765f.jpg'
+
 
 function App() {
+ 
+ 
+  const [showPopup, setShowPopup] = useState(true);
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  };
+
   return (
+    <>
+    <div>
+        {/* Your main application content */}
+        <DeveloperInfoPopup
+          show={showPopup}
+          onClose={handleClosePopup}
+          studentName="Soham prabhu"
+          studentPhotoUrl="src\assets\Screenshot 2025-06-13 165827.png" // Path to their photo
+          uniqueMessage="Learned so much during this OJT! This app showcases my independent coding and deployment skills"
+        />
+      </div>
     <BrowserRouter>
       <div className="main-layout">
         <Header />
@@ -36,7 +57,10 @@ function App() {
         <Footer />
       </div>
     </BrowserRouter>
+    </>
   );
+  
 }
+
 
 export default App;
